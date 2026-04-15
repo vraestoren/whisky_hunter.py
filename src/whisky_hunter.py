ec03e680c87bf83f335be8d308013d0cd03a8eec
huok <77536370+vraestoren@users.noreply.github.com>
2026-04-15 12:46:05 +0300

    Update whisky_hunter.py

--- src/whisky_hunter.py
+++ src/whisky_hunter.py
@@ -7,24 +7,23 @@ class WhiskyHunter:
 		self.session.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
 		}
+
+	def _get(self, endpoint: str) -> dict:
+		return self.session.get(f"{self.api}{endpoint}").json()
 	
 	def read_auction_data(self, slug: str) -> dict:
-		return self.session.get(
-			f"{self.api}/auction_data/{slug}/auction_data_read").json()
+		return self._get(
+			f"/auction_data/{slug}/auction_data_read")
 	
 	def get_auctions_data(self) -> dict:
-		return self.session.get(
-			f"{self.api}/auctions_data/auctions_data_list").json()
+		return self._get("/auctions_data/auctions_data_list")
 	
 	def get_auctions_info(self) -> dict:
-		return self.session.get(
-			f"{self.api}/auctions_info/auctions_info_list").json()
+		return self._get("/auctions_info/auctions_info_list")
 	
 	def get_distilleries_info(self) -> dict:
-		return self.session.get(
-			f"{self.api}/distilleries_info/distilleries_info_list").json()
+		return self._get("/distilleries_info/distilleries_info_list")
 	
 	def read_distillery_data(self, slug: str) -> dict:
-		return self.session.get(
-			f"{self.api}/distillery_data/{slug}/distillery_data_read").json()
-			
+		return self._get(
+			f"/distillery_data/{slug}/distillery_data_read")
